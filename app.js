4d5699440b2119ed5a38155671ff9d2b9f59846a
Polina Chetnikova <38739507+pchova@users.noreply.github.com>
2024-07-18 10:34:26 -0700

    added new grid button and styling, added h2

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ let numOfDivs = 16;
 /* SELECTORS */
 const container = document.querySelector(".container");
 
+const btnContainer = document.querySelector("#gridBtn");
+const btn = document.createElement("button");
+btn.textContent = "NEW GRID";
+btn.classList.add("newGridBtn");
+btnContainer.appendChild(btn);
+
 /* CLASSES */
 function createDiv() {
     for (let i = 1; i <= numOfDivs; i++) {
