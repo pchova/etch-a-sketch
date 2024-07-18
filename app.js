18cc3201dabe04ceaba424000928fa0ee41f4b80
Polina Chetnikova <38739507+pchova@users.noreply.github.com>
2024-07-18 10:07:18 -0700

    added flexbox to css .divStyle and .container so divs are evenly divided in container

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const container = document.querySelector(".container");
 
 /* CLASSES */
 function createDiv() {
-    for (let i = 0; i < numOfDivs; i++) {
+    for (let i = 1; i <= numOfDivs; i++) {
         const newDiv = document.createElement('div');
+        newDiv.textContent = i;
         newDiv.setAttribute('class', 'divStyle');
         container.appendChild(newDiv);
     }
